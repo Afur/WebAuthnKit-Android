4f73d7a7b1d141ae3c081f84ed37b74b176a91f0
Afur <teodorowiczartur@gmail.com>
2021-09-23 21:42:34 +0200

    Remove user confirmation dialog

--- webauthnkit/src/main/kotlin/webauthnkit/core/authenticator/internal/ui/DefaultUserConsentUI.kt
+++ webauthnkit/src/main/kotlin/webauthnkit/core/authenticator/internal/ui/DefaultUserConsentUI.kt
@@ -6,6 +6,7 @@ import android.app.KeyguardManager
 import android.content.Context.KEYGUARD_SERVICE
 import android.content.Intent
 import android.os.Build
+import android.text.format.DateFormat
 import androidx.fragment.app.FragmentActivity
 
 import kotlin.coroutines.resume
@@ -20,13 +21,14 @@ import webauthnkit.core.authenticator.internal.ui.dialog.*
 import webauthnkit.core.error.*
 import webauthnkit.core.util.WAKLogger
 import webauthnkit.core.data.*
+import java.util.*
 
 @ExperimentalUnsignedTypes
 @ExperimentalCoroutinesApi
 @TargetApi(Build.VERSION_CODES.M)
 class DefaultUserConsentUI(
     private val activity: FragmentActivity
-): UserConsentUI {
+) : UserConsentUI {
 
     companion object {
         val TAG = DefaultUserConsentUI::class.simpleName
@@ -59,41 +61,16 @@ class DefaultUserConsentUI(
                     it.onFailed()
                 }
             }
-
             keyguardResultListener = null
-
             true
 
         } else {
-
             false
-
         }
-    }
-
-    private fun onStartUserInteraction() {
-        isOpen = true
-        cancelled = null
     }
 
     private fun <T> finish(cont: Continuation<T>, result: T) {
-        WAKLogger.d(TAG, "finish")
-        isOpen = false
-        if (cancelled != null) {
-            cont.resumeWithException(cancelled!!.rawValue)
-        } else {
-            cont.resume(result)
-        }
-    }
-
-    private fun <T> fail(cont: Continuation<T>) {
-        WAKLogger.d(TAG, "fail")
-        isOpen = false
-        if (cancelled != null) {
-            cont.resumeWithException(cancelled!!.rawValue)
-        } else {
-            cont.resumeWithException(CancelledException())
-        }
+        cont.resume(result)
     }
 
     override fun cancel(reason: ErrorReason) {
@@ -101,147 +78,22 @@ class DefaultUserConsentUI(
     }
 
     override suspend fun requestUserConsent(
-        rpEntity:                PublicKeyCredentialRpEntity,
-        userEntity:              PublicKeyCredentialUserEntity,
+        rpEntity: PublicKeyCredentialRpEntity,
+        userEntity: PublicKeyCredentialUserEntity,
         requireUserVerification: Boolean
     ): String = suspendCoroutine { cont ->
-
-        WAKLogger.d(TAG, "requestUserConsent")
-
-        onStartUserInteraction()
-
-        activity.runOnUiThread {
-
-            WAKLogger.d(TAG, "requestUserConsent switched to UI thread")
-
-            // TODO make this configurable
-            val dialog = DefaultRegistrationConfirmationDialog(config)
-
-            dialog.show(activity, rpEntity, userEntity, object :
-                RegistrationConfirmationDialogListener {
-
-                override fun onCreate(keyName: String) {
-                    if (requireUserVerification) {
-                        showKeyguard(cont, keyName)
-                    } else {
-                        finish(cont, keyName)
-                    }
-                }
-
-                override fun onCancel() {
-                    fail(cont)
-                }
-
-            })
-
-        }
+        finish(cont, getDefaultKeyName(userEntity.name))
     }
 
     override suspend fun requestUserSelection(
-        sources:                 List<PublicKeyCredentialSource>,
+        sources: List<PublicKeyCredentialSource>,
         requireUserVerification: Boolean
     ): PublicKeyCredentialSource = suspendCoroutine { cont ->
-
-        WAKLogger.d(TAG, "requestUserSelection")
-
-        onStartUserInteraction()
-
-        activity.runOnUiThread {
-
-            if (sources.size == 1 && !config.alwaysShowKeySelection) {
-
-                WAKLogger.d(TAG, "found 1 source, skip selection")
-
-                executeSelectionVerificationIfNeeded(
-                    requireUserVerification = requireUserVerification,
-                    source                  = sources[0],
-                    cont                    = cont
-                )
-
-            } else {
-
-                WAKLogger.d(TAG, "show selection dialog")
-
-                val dialog = DefaultSelectionConfirmationDialog(config)
-
-                dialog.show(activity, sources, object :
-                    SelectionConfirmationDialogListener {
-
-                    override fun onSelect(source: PublicKeyCredentialSource) {
-                        WAKLogger.d(TAG, "selected")
-                        executeSelectionVerificationIfNeeded(
-                            requireUserVerification = requireUserVerification,
-                            source                  = source,
-                            cont                    = cont
-                        )
-                    }
-
-                    override fun onCancel() {
-                        WAKLogger.d(TAG, "canceled")
-                        fail(cont)
-                    }
-                })
-
-            }
-        }
+        finish(cont, sources.first())
     }
 
-    private fun executeSelectionVerificationIfNeeded(
-        requireUserVerification: Boolean,
-        source:                  PublicKeyCredentialSource,
-        cont:                    Continuation<PublicKeyCredentialSource>
-    ) {
-        if (requireUserVerification) {
-            showKeyguard(cont, source)
-        } else {
-            finish(cont, source)
-        }
-    }
-
-    private fun <T> showKeyguard(cont: Continuation<T>, consentResult: T) {
-
-        WAKLogger.d(TAG, "showKeyguard")
-
-        val keyguardManager =
-            activity.getSystemService(KEYGUARD_SERVICE) as KeyguardManager
-
-        if (!keyguardManager.isKeyguardSecure) {
-            WAKLogger.d(TAG, "keyguard is not secure")
-
-            showErrorDialog(cont, config.messageKeyguardNotSetError)
-
-        } else {
-            WAKLogger.d(TAG, "keyguard is secure")
-
-            keyguardResultListener = object : KeyguardResultListener {
-
-                override fun onAuthenticated() {
-                    WAKLogger.d(TAG, "keyguard authenticated")
-                    finish(cont, consentResult)
-                }
-
-                override fun onFailed() {
-                    WAKLogger.d(TAG, "failed keyguard authentication")
-                    fail(cont)
-                }
-            }
-
-            val intent =
-                keyguardManager.createConfirmDeviceCredentialIntent(
-                    config.messageKeyguardTitle, config.messageKeyguardDescription)
-            activity.startActivityForResult(intent, REQUEST_CODE)
-        }
-    }
-
-    private fun <T> showErrorDialog(cont: Continuation<T>, reason: String) {
-
-        val dialog = VerificationErrorDialog(config)
-
-        dialog.show(activity, reason, object: VerificationErrorDialogListener {
-            override fun onComplete() {
-                fail(cont)
-            }
-        })
-
+    private fun getDefaultKeyName(username: String): String {
+        val date = DateFormat.format("yyyyMMdd", Calendar.getInstance())
+        return "$username($date)"
     }
 }
